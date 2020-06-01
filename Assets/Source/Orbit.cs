5dbdbbcf8f9b683a0212103c46bade81af407e7c
ferdelmo <ferdelmo1996@gmail.com>
2020-06-01 13:22:00 +0200

    Gravity and orbits

---
+++ Assets/Source/Orbit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Component that curved a ball position
+ */
+public class Orbit : MonoBehaviour
+{
+
+    public float mass = 1.0f;
+
+    private float radius = 5.0f;
+
+    public float Radius {
+        get { return radius; }
+    }
+
+    private CircleCollider2D outter;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(CircleCollider2D cc in GetComponents<CircleCollider2D>())
+        {
+            if (cc.isTrigger)
+            {
+                radius = cc.radius;
+                outter = cc;
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
