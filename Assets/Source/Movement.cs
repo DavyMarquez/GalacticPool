631f7c80667222c3619ffa9e26a9717c11626164
DavyMarquez <davidmarquezcalavia@gmail.com>
2020-06-01 12:20:24 +0200

    filling up some folders

---
+++ Assets/Source/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float restitution = 0.0f;
+
+    private Rigidbody2D rigidbody;
+
+    /* Position of the mouse when clicked and released to 
+    / calculate force to apply */
+    private Vector3 _mousePosDown;
+    private Vector3 _mousePosUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //_mousePosDown = 
+    }
+}
